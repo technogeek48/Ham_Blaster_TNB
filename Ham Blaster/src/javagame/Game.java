b7e80119638389e0e798b0bc8e2c161e5890554b
Michael-John Sakellaropoulos <michael.john.tech@gmail.com>
2012-05-29 16:40:22 -0400

    started coding

--- Ham Blaster/src/javagame/Game.java
+++ Ham Blaster/src/javagame/Game.java
@@ -1,12 +1,30 @@
 package javagame;
 
 //Imports from slick
+import java.awt.DisplayMode;
+
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
 public class Game extends StateBasedGame {
 	
+	public static final String gamename = "Ham Blaster 2.0!";
+	public static final int menu = 0;
+	public static final int play = 1;
+
 	public static void main(String[] args){
+		
+	}
+	
+	public Game(){
+		super (gamename);
+		this.addState(new Menu(menu));
+		this.addState(new Play(play));
+		
+	}
+
+	public void initStatesList(GameContainer arg0) throws SlickException {
+		
 		
 	}
 	
