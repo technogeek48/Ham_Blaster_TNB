33a2f45becbd460114ccad774f7021bb85c23a85
Michael-John Sakellaropoulos <michael.john.tech@gmail.com>
2012-05-29 10:03:20 -0400

    added stuffs again

--- Ham Blaster/src/javagame/Game.java
+++ Ham Blaster/src/javagame/Game.java
@@ -4,7 +4,7 @@ package javagame;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
-public class Game {
+public class Game extends StateBasedGame {
 	
 	public static void main(String[] args){
 		
